d3ca356ceb3e9a35ee6eb58b21e0aa7d214d3923
rekabuku <39343688+rekabuku@users.noreply.github.com>
2018-08-02 11:20:56 +0200

    added notes

--- week-01/day-04/first/src/Main.java
+++ week-01/day-04/first/src/Main.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Main {
+    public static void main (String[] args) {
+        /*
+        Scanner sc = new Scanner (System.in);
+        String line = sc.nextLine();
+        System.out.println("Hello" + line);
+        */
+
+    
+
+    }
+
 }
+
