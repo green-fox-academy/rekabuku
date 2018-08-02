a9454e7135715b1e91cb44436abc87b4e8f1ebf0
rekabuku <39343688+rekabuku@users.noreply.github.com>
2018-08-02 17:00:33 +0200

    finish task13

--- week-01/day-04/13-SecondsInADay/src/com/company/Main.java
+++ week-01/day-04/13-SecondsInADay/src/com/company/Main.java
@@ -3,6 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	
+        int currentHours = 14;
+        int currentMinutes = 34;
+        int currentSeconds = 42;
+        int remainingHours = 59 - currentHours;
+        int remainingMinutes = 59 - currentMinutes;
+        int remainingSeconds = 60 - currentSeconds;
+        int remaining = remainingSeconds + (remainingMinutes * 60) + (remainingHours * 60 * 60);
+        System.out.println(remaining);
+
+        // Write a program that prints the remaining seconds (as an integer) from a
+        // day if the current time is represented by the variables
     }
 }
