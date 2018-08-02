dd6610d876361b01e09465c6031e9e7d2be8c0e9
rekabuku <39343688+rekabuku@users.noreply.github.com>
2018-08-02 15:19:23 +0200

    finish

---
+++ week-01/day-04/10-DefineBasicInfo/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Define several things as a variable then print their values
+        // Your name as a string
+        String name = "Reka";
+        System.out.println(name);
+        // Your age as an integer
+        int age = 30;
+        System.out.println(age);
+        // Your height in meters as a double
+        double height = 1.7;
+        System.out.println(height);
+        // Whether you are married or not as a boolean
+        boolean married = true;
+        System.out.println(!true);
+    }
+}
