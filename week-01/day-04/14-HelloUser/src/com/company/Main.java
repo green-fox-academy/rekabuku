66d3c56a83a8092034566bf5fad76728ad7c6f65
rekabuku <39343688+rekabuku@users.noreply.github.com>
2018-08-02 17:05:14 +0200

    finish task14

---
+++ week-01/day-04/14-HelloUser/src/com/company/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Modify this program to greet user instead of the World!
+        // The program should ask for the name of the user
+        Scanner sc = new Scanner (System.in);
+        String line = sc.nextLine();
+        System.out.println("Hello" + line);
+    }
+}
