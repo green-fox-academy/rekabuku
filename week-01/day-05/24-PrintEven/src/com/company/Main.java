53b8eb0b1276f08d81035172c31273970384bdec
rekabuku <39343688+rekabuku@users.noreply.github.com>
2018-08-03 14:16:13 +0200

    finish task 24

---
+++ week-01/day-05/24-PrintEven/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+// Create a program that prints all the even numbers between 0 and 500
+
+        String name = "I wont cheat on the exam!";
+        int i = 0;
+        while (i < 500){
+        if (i % 2 == 0)
+
+            System.out.println(i);
+            i++; }
+
+
+
+
+    }
+}
