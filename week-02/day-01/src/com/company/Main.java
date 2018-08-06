d9dc13ffc3db9e9f22f780dce9e3f801187696a8
rekabuku <39343688+rekabuku@users.noreply.github.com>
2018-08-06 15:03:02 +0200

    finish Doubling

---
+++ week-02/day-01/src/com/company/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+// - Create an integer variable named `baseNum` and assign the value `123` to it
+// - Create a function called `doubling` that doubles it's input parameter and returns with an integer
+// - Print the result of `doubling(baseNum)`
+        int baseNum = 123;
+        System.out.println(Doubling(baseNum));
+    }
+
+
+        public static int Doubling(int i) {
+            return i *2;
+        }
+
+
+    }
