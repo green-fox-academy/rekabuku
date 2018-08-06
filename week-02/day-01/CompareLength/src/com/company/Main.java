931c9e3792ca2ca3d4ad06d5b72769be0078473e
rekabuku <39343688+rekabuku@users.noreply.github.com>
2018-08-06 17:35:09 +0200

    finish CompareLength

---
+++ week-02/day-01/CompareLength/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int [] p1 = {1, 2, 3,};
+    int [] p2 = {4, 5,};
+    boolean b = p1.length < p2.length;
+        System.out.println(b);
+    }
+}
