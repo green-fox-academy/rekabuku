0b16d4fc75eebd373df7b340c54c93ebaa0b7583
rekabuku <39343688+rekabuku@users.noreply.github.com>
2018-08-06 16:18:31 +0200

    finish Sum

--- week-02/day-01/Sum/src/com/company/Main.java
+++ week-02/day-01/Sum/src/com/company/Main.java
@@ -1,8 +1,17 @@
-package com.company;
+
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int num = 5;
+        int sum = summerized(num);
+        System.out.println(sum);
+    }
+    public static int summerized (int a){
+        int sum1 = 0;
+        for (int i = 0; i <= a; i++)
+
+        sum1 += i;
+        return sum1;
     }
 }
