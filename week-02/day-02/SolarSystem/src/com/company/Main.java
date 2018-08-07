47f51668a30a342706278de1110738a49b70d505
rekabuku <39343688+rekabuku@users.noreply.github.com>
2018-08-07 16:44:06 +0200

    finish SolarSystem

--- week-02/day-02/SolarSystem/src/com/company/Main.java
+++ week-02/day-02/SolarSystem/src/com/company/Main.java
@@ -1,16 +1,27 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static sun.security.jca.ProviderList.newList;
+
 public class Main {
 
     public static void main(String[] args) {
-        public static void main(String... args){
-            ArrayList<String> planetList = new ArrayList<String>(Arrays.asList("Mercury","Venus","Earth","Mars","Jupiter","Uranus","Neptune"));
 
-            // Saturn is missing from the planetList
-            // Insert it into the correct position
-            // Create a method called putSaturn() which has list parameter and returns the correct list.
 
-            System.out.println(putSaturn(planetList));
-            // Expected output: "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune", "Saturn"
+        ArrayList<String> planetList = new ArrayList<String>(Arrays.asList("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune"));
+        System.out.println(ammended(planetList));
+
     }
+
+    public static ArrayList ammended(ArrayList planetList) {
+        planetList.add(7, "Saturn");
+
+        return planetList;
+
+
+    }
+    // Expected output: "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune", "Saturn"
 }
+
