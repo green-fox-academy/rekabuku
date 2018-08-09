0ea8f0973a8b93c688ce0896668d951674bc4e19
rekabuku <39343688+rekabuku@users.noreply.github.com>
2018-08-09 15:25:48 +0200

    finish Increment

---
+++ week-02/day-04/IncrementElement/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Create an array variable named `t`
+        //with the following content: `[1, 2, 3, 4, 5]`
+        // Increment the third element
+        //Print the third element
+
+        int [] t = {1, 2, 3, 4, 5};
+        t[2] = t[2]+3;
+        System.out.println(t [2]);
+    }
+}
