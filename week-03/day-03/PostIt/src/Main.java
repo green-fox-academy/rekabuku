e7dbac2a86ecdfee5687601ecc61a6a7931ce7b7
rekabuku <39343688+rekabuku@users.noreply.github.com>
2018-08-15 13:13:15 +0200

    finish PostIt task

---
+++ week-03/day-03/PostIt/src/Main.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+
+public class Main {
+    /*Create a PostIt class that has
+            a backgroundColor
+            a text on it
+            a textColor
+            Create a few example post-it objects:
+    an orange with blue text: "Idea 1"
+    a pink with black text: "Awesome"
+    a yellow with green text: "Superb!"*/
+
+    PostIt post1 = new PostIt(Color.orange, "Idea 1", Color.blue);
+    PostIt post2 = new PostIt(Color.pink, "Awesome", Color.black);
+    PostIt post3 = new PostIt(Color.yellow, "Superb!", Color.green);
+
+}
