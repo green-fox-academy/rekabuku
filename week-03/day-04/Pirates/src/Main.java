4bb32def1cfd579d489ea8077018bd86a7343cad
rekabuku <39343688+rekabuku@users.noreply.github.com>
2018-08-16 11:53:48 +0200

    created Pirate class

---
+++ week-03/day-04/Pirates/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    Pirate pirate1 = new Pirate("Jack", 100,"captain",32);
+    Pirate pirate2 = new Pirate("John", 100,"sailor",33);
+    Pirate pirate3 = new Pirate("Jim", 100,"sailor",34);
+    Pirate pirate4 = new Pirate("Joe", 100,"sailor",35);
+    Pirate pirate5 = new Pirate("Phil", 100,"sailor",36);
+    Pirate pirate6 = new Pirate("Larry", 100,"sailor",37);
+    Pirate pirate7 = new Pirate("Bob", 100,"sailor",25);
+    Pirate pirate8 = new Pirate("Mickey", 100,"sailor",26);
+    Pirate pirate9 = new Pirate("Mike", 100,"sailor",27);
+    Pirate pirate10 = new Pirate("Sam", 100,"sailor",28);
+
+
+
+
+}
---
+++ week-03/day-04/Pirates/src/Pirate.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Pirate {
+    String name;
+    int lifeScore;
+    String title;
+    int age;
+    static ArrayList
+
+    Pirate(String name, int lifeScore,String title, int age) {
+        this.name = name;
+        this.lifeScore = lifeScore;
+        this.title = title;
+        this.age = age;
+
+    }
+
+
+
+}
