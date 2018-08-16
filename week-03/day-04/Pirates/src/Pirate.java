76d85d6ef14c0806281e9675fb53570ac44550b5
rekabuku <39343688+rekabuku@users.noreply.github.com>
2018-08-16 13:25:49 +0200

    updated 2 methods

--- week-03/day-04/Pirates/src/Pirate.java
+++ week-03/day-04/Pirates/src/Pirate.java
@@ -1,20 +1,32 @@
-import java.util.ArrayList;
-
 public class Pirate {
     String name;
     int lifeScore;
     String title;
     int age;
-    static ArrayList
+    int drunk;
 
-    Pirate(String name, int lifeScore,String title, int age) {
+
+
+    Pirate(String name, int lifeScore, String title, int age, int drunk) {
         this.name = name;
         this.lifeScore = lifeScore;
         this.title = title;
         this.age = age;
+        this.drunk = drunk;
 
     }
 
+    void fight() {
+        this.lifeScore--;
+    }
 
+    void drink() {
+        this.lifeScore--;
+        this.drunk++;
+    }
+
+    boolean isAlive() {
+        return this.lifeScore > 0;
+    }
 
 }
