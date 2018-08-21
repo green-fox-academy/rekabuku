dbb6cbc3b70fc58b23a9aa7064de3b5da28eb5bd
rekabuku <39343688+rekabuku@users.noreply.github.com>
2018-08-21 15:15:56 +0200

    finish Mentor class

--- week-04/day-02/GreenFox/src/Mentor.java
+++ week-04/day-02/GreenFox/src/Mentor.java
@@ -3,8 +3,24 @@ import com.company.Person;
 public class Mentor extends Person {
     private String level;
 
+    public Mentor(String name, int age, String gender, String level) {
+        super(name, age, gender);
+        this.level = level;
+    }
+
+    public Mentor() {
+        this.level = "intermediate";
+    }
+
     @Override
     public void getGoal() {
-        System.out.println();
+        System.out.println("Educate brilliant junior software developers.");
     }
+
+    @Override
+    public void introduce() {
+        System.out.println("Hi, I'm " + getName() + ", a " + getAge() + " year old " + getGender() + " mentor.");
+    }
+
+
 }
