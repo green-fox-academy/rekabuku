7f18e26641903a9848fae0a4e4b3d447546ade2f
rekabuku <39343688+rekabuku@users.noreply.github.com>
2018-09-10 17:40:57 +0200

    finish task8

---
+++ week-06/day-01/Exercise8/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2));
+
+        Map<Integer, Long> frequency = numbers.stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        System.out.println("Frequencies:\n" + frequency);
+    }
+}
