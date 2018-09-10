24dc7d9ed62765e5717684e9fd6a1610dc0d626d
rekabuku <39343688+rekabuku@users.noreply.github.com>
2018-09-10 14:48:36 +0200

    finish task2

--- week-06/day-01/Exercise2/src/Main.java
+++ week-06/day-01/Exercise2/src/Main.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.DoubleSummaryStatistics;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
 
 public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
+        OptionalDouble oddNumbers = numbers.stream()
+                .mapToDouble(number -> number)
+                .filter(number -> number % 2 != 0)
+                .average();
+        System.out.println(oddNumbers);
+    }
 }
