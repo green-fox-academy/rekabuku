383c03c75a09843ad263b1b35b834f3b864c6876
rekabuku <39343688+rekabuku@users.noreply.github.com>
2018-09-17 14:14:48 +0200

    finished AtomitLong task

--- week-07/helloworld/src/main/java/com/greenfoxacademy/helloworld/controllers/RESTGreetCounterApp.java
+++ week-07/helloworld/src/main/java/com/greenfoxacademy/helloworld/controllers/RESTGreetCounterApp.java
@@ -4,10 +4,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 public class RESTGreetCounterApp {
+    AtomicLong id = new AtomicLong(0);
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam("name") String name){
-        return new Greeting(1, name);
+        return new Greeting( id.incrementAndGet(), name);
     }
 }
