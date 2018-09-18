859c8baada7fe1cab1de91804155c4d797d751a2
rekabuku <39343688+rekabuku@users.noreply.github.com>
2018-09-18 13:27:53 +0200

    added class and html

---
+++ week-07/simba/src/main/java/com/greenfoxacademy/simba/simba/model/BankAccount.java
@@ -0,0 +1,37 @@
+package com.greenfoxacademy.simba.simba.model;
+
+public class BankAccount {
+   private String name;
+    private int balance;
+    private String animalType;
+
+    public BankAccount(String name, int balance, String animalType) {
+        this.name = name;
+        this.balance = balance;
+        this.animalType = animalType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public String getAnimalType() {
+        return animalType;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public void setAnimalType(String animalType) {
+        this.animalType = animalType;
+    }
+}
