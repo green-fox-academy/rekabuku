78310da00cf4a5532740d47e11c36d5018f07ee4
rekabuku <39343688+rekabuku@users.noreply.github.com>
2018-09-18 13:45:47 +0200

    finish first part of task

--- week-07/simba/src/main/java/com/greenfoxacademy/simba/simba/controllers/Show.java
+++ week-07/simba/src/main/java/com/greenfoxacademy/simba/simba/controllers/Show.java
@@ -1,11 +1,19 @@
 package com.greenfoxacademy.simba.simba.controllers;
 
+import com.greenfoxacademy.simba.simba.model.BankAccount;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Show {
 
-    @RequestMapping("/show");
+    @RequestMapping("/show")
+    public String show (Model model){
+        model.addAttribute("account" ,new BankAccount("Simba", 2000, "lion"));
+        return "show";
+    }
+
 
 }
