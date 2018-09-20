800d6e98c0a9fb02ba6a7750525b38f747837318
rekabuku <39343688+rekabuku@users.noreply.github.com>
2018-09-20 18:16:10 +0200

    added css to info page

--- week-07/day-04/src/main/java/com/greenfoxacademy/foxclub/controllers/MainController.java
+++ week-07/day-04/src/main/java/com/greenfoxacademy/foxclub/controllers/MainController.java
@@ -1,13 +1,16 @@
 package com.greenfoxacademy.foxclub.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
     @RequestMapping("/index")
-    public String info() {
+    public String getName(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
         return "index";
     }
 }
