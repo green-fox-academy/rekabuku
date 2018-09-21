9fc1b63f425cf7992a1f4043f605a6f589c3a787
rekabuku <39343688+rekabuku@users.noreply.github.com>
2018-09-21 14:18:52 +0200

    added nutrition html

--- week-07/day-04/src/main/java/com/greenfoxacademy/foxclub/controllers/NutritionController.java
+++ week-07/day-04/src/main/java/com/greenfoxacademy/foxclub/controllers/NutritionController.java
@@ -3,6 +3,7 @@ package com.greenfoxacademy.foxclub.controllers;
 import com.greenfoxacademy.foxclub.services.FoxService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class NutritionController {
@@ -10,6 +11,11 @@ public class NutritionController {
 
     public NutritionController(FoxService foxService) {
         this.foxService = foxService;
+    }
+
+    @RequestMapping("/nutrition")
+    public String trick(){
+        return "nutrition";
     }
 
 }
