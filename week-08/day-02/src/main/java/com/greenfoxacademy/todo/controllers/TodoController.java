c6511764918852bf2b0bf46f4df2293fee8037be
rekabuku <39343688+rekabuku@users.noreply.github.com>
2018-09-25 17:32:35 +0200

    added create function

--- week-08/day-02/src/main/java/com/greenfoxacademy/todo/controllers/TodoController.java
+++ week-08/day-02/src/main/java/com/greenfoxacademy/todo/controllers/TodoController.java
@@ -1,9 +1,11 @@
 package com.greenfoxacademy.todo.controllers;
 
+import com.greenfoxacademy.todo.models.Todo;
 import com.greenfoxacademy.todo.repositories.TodoRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -27,5 +29,22 @@ public class TodoController {
         model.addAttribute("todos", todoRepository.findAll());
         model.addAttribute("done", done);
         return "activetodo";
+    }
+
+    @GetMapping("/todo/add")
+    public String addTodo(){
+        return "add";
+    }
+
+    @PostMapping("/todo/add")
+    public String getName(@RequestParam ("todo") String todo){
+        if(todo.isEmpty()){
+            return "add";
+
+        }
+        Todo newTodo = new Todo();
+        newTodo.setTitle(todo);
+        todoRepository.save(newTodo);
+        return "redirect:/todo";
     }
 }
