1b03fb2b09dcbc3c5fcd70f49856bbd746691ed2
rekabuku <39343688+rekabuku@users.noreply.github.com>
2018-09-25 15:16:02 +0200

    filled table with todos

--- week-08/day-02/src/main/java/com/greenfoxacademy/todo/TodoApplication.java
+++ week-08/day-02/src/main/java/com/greenfoxacademy/todo/TodoApplication.java
@@ -23,8 +23,27 @@ public class TodoApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
         Todo todo = new Todo();
         todo.setTitle("Drink coffee");
+        Todo todo2 = new Todo();
+        todo2.setTitle("Clean the bathroom");
+        todo2.setUrgent(true);
+        Todo todo3 = new Todo();
+        todo3.setTitle("Buy milk");
+        todo3.setDone(true);
+        Todo todo4 = new Todo();
+        todo4.setTitle("Pay the bills");
+        todo4.setUrgent(true);
+        todo4.setDone(true);
+        Todo todo5 = new Todo();
+        todo5.setTitle("Order pizza");
+        Todo todo6 = new Todo();
+        todo6.setTitle("Go to bed");
 
         todoRepository.save(todo);
+        todoRepository.save(todo2);
+        todoRepository.save(todo3);
+        todoRepository.save(todo4);
+        todoRepository.save(todo5);
+        todoRepository.save(todo6);
         System.out.println(todoRepository.findAll());
 
     }
