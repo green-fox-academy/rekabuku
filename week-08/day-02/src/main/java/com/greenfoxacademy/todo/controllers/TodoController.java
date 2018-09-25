377d0b600d81d02eabcc51321578914247f043aa
rekabuku <39343688+rekabuku@users.noreply.github.com>
2018-09-25 16:47:40 +0200

    listed active todos

--- week-08/day-02/src/main/java/com/greenfoxacademy/todo/controllers/TodoController.java
+++ week-08/day-02/src/main/java/com/greenfoxacademy/todo/controllers/TodoController.java
@@ -3,7 +3,9 @@ package com.greenfoxacademy.todo.controllers;
 import com.greenfoxacademy.todo.repositories.TodoRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TodoController {
@@ -13,9 +15,17 @@ public class TodoController {
         this.todoRepository = todoRepository;
     }
 
-    @RequestMapping(value= {"/todo", "/todo/list"})
+    @GetMapping("/todo")
     public String list(Model model){
         model.addAttribute("todos", todoRepository.findAll());
         return "todolist";
+    }
+
+
+    @RequestMapping(value= {"/todo/list"})
+    public String list(@RequestParam ("isActive")Boolean done, Model model){
+        model.addAttribute("todos", todoRepository.findAll());
+        model.addAttribute("done", done);
+        return "activetodo";
     }
 }
