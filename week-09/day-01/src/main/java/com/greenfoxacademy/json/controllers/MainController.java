79ecfe6ad7a808d1106c48f91d037558fdd509af
rekabuku <39343688+rekabuku@users.noreply.github.com>
2018-10-01 14:36:45 +0200

    finish greeter

--- week-09/day-01/src/main/java/com/greenfoxacademy/json/controllers/MainController.java
+++ week-09/day-01/src/main/java/com/greenfoxacademy/json/controllers/MainController.java
@@ -1,6 +1,7 @@
 package com.greenfoxacademy.json.controllers;
 
 import com.greenfoxacademy.json.model.Doubling;
+import com.greenfoxacademy.json.model.Greeting;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,14 +18,28 @@ public class MainController {
     @GetMapping("/doubling")
     @ResponseBody
     public Doubling doublingMethod(@RequestParam("input") Long input) {
-       Doubling doubling = new Doubling();
-       if (input == null){
-           doubling.setError("Please provide an input!");
-       return doubling;
-       }
-       doubling.setReceived(input);
-       doubling.setResult(input*2);
-       return doubling;
+        Doubling doubling = new Doubling();
+        if (input == null) {
+            doubling.setError("Please provide an input!");
+            return doubling;
+        }
+        doubling.setReceived(input);
+        doubling.setResult(input * 2);
+        return doubling;
     }
 
+    @GetMapping("/greeter")
+    @ResponseBody
+    public Greeting greetingMethod(@RequestParam("name") String name, @RequestParam("title") String title) {
+        if (name.isEmpty() && title.isEmpty()) {
+            return new Greeting("null", "Please provide a name and a title!");
+        }
+            if (name.isEmpty()) {
+                return new Greeting("null", "Please provide a name!");
+        }
+        if (title.isEmpty()) {
+            return new Greeting("null", "Please provide a title!");
+        }
+        return new Greeting("Oh, hi there " + name + ", my dear " + title);
+    }
 }
