88d0ec81f932642b2360bde4afdcabeed89ed88b
rekabuku <39343688+rekabuku@users.noreply.github.com>
2018-10-01 13:53:22 +0200

    added doubling

--- week-09/day-01/src/main/java/com/greenfoxacademy/json/controllers/MainController.java
+++ week-09/day-01/src/main/java/com/greenfoxacademy/json/controllers/MainController.java
@@ -1,12 +1,26 @@
 package com.greenfoxacademy.json.controllers;
 
-import org.springframework.ui.Model;
+import com.greenfoxacademy.json.model.Doubling;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 public class MainController {
 
     @GetMapping("/")
     public String displayMain() {
         return "index";
     }
+
+    @GetMapping("/doubling")
+    @ResponseBody
+    public Doubling doublingMethod(@RequestParam("input") long input) {
+       Doubling doubling = new Doubling();
+       doubling.setReceived(input);
+       doubling.setResult(input*2);
+       return doubling;
+    }
+
 }
---
+++ week-09/day-01/src/main/java/com/greenfoxacademy/json/model/Doubling.java
@@ -0,0 +1,27 @@
+package com.greenfoxacademy.json.model;
+
+public class Doubling {
+    private long received;
+    private long result;
+
+    public Doubling() {
+        this.received = received;
+        this.result = result;
+    }
+
+    public long getReceived() {
+        return received;
+    }
+
+    public void setReceived(long received) {
+        this.received = received;
+    }
+
+    public long getResult() {
+        return result;
+    }
+
+    public void setResult(long result) {
+        this.result = result;
+    }
+}
